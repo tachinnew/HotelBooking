26ad7c13aa5e63b2025fccd04b966a9f101cddc1
DevChin <tachinnew@outlook.com>
2023-09-14 21:37:04 +0700

    addtemparray

--- temp.py
+++ temp.py
@@ -1,10 +1,18 @@
+import allRoom as room
+
 tempmemory = ""
 
+delete_array = {}
 temp_array = {}
 
-def add_array():
-    temp_array.append(temp_array)
-    
+def add_array(tmp):
+    temp_array.append(tmp)
+
+def check_true():
+    temp_array = delete_array
+    for element in room.roomAll:
+        if room.roomStatus[element]:
+            add_array(element)
 
 def set_temp(text):
     tempmemory = text
